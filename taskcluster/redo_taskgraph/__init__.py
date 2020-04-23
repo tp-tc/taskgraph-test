b805e6f1de96fe559c196207c59b31ae1db3dfb6
Tom Prince <mozilla@hocat.ca>
2020-04-23 14:26:55 -0600

    upload

--- taskcluster/redo_taskgraph/__init__.py
+++ taskcluster/redo_taskgraph/__init__.py
@@ -9,6 +9,15 @@ from pipes import quote as shell_quote
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.templates import merge
 
+from six import text_type
+from voluptuous import Required
+from taskgraph.util.schema import taskref_or_string
+from taskgraph.transforms.task import payload_builder
+
+
+def register(graph_config):
+    pass
+
 
 transforms = TransformSequence()
 
@@ -39,3 +48,48 @@ def fill_template(config, tasks):
         taskdesc = merge(task, taskdesc)
 
         yield taskdesc
+
+
+@payload_builder(
+    "scriptworker-pypi",
+    schema={
+        # the maximum time to run, in seconds
+        Required("max-run-time"): int,
+        Required("action"): text_type,
+        # list of artifact URLs for the artifacts that should be signed
+        Required("upstream-artifacts"): [
+            {
+                # taskId of the task with the artifact
+                Required("taskId"): taskref_or_string,
+                # type of signing task (for CoT)
+                Required("taskType"): text_type,
+                # Paths to the artifacts to sign
+                Required("paths"): [text_type],
+                # Signing formats to use on each of the paths
+                Required("project"): text_type,
+            }
+        ],
+    },
+)
+def build_scriptworker_signing_payload(config, task, task_def):
+    worker = task["worker"]
+
+    task_def["tags"]["worker-implementation"] = "scriptworker"
+
+    task_def["payload"] = {
+        "maxRunTime": worker["max-run-time"],
+        "upstreamArtifacts": worker["upstream-artifacts"],
+        "action": worker['action'],
+    }
+
+    projects = set()
+    for artifacts in worker["upstream-artifacts"]:
+        projects.add(artifacts["project"])
+
+    scope_prefix = config.graph_config["scriptworker"]["scope-prefix"]
+    task_def["scopes"].extend(
+        [
+            "{}:pypi:project:{}".format(scope_prefix, project)
+            for project in sorted(projects)
+        ]
+    )
